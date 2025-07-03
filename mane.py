0c4259ddce4482891c00fe8f653e3b76a0b993c9
Yaakov <y0583275590@gmail.com>
2025-07-03 12:42:40 +0300

    I added the table and graph functionality.

--- mane.py
+++ mane.py
@@ -1,7 +1,23 @@
+import plotly.express as px
 import  streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.set_page_config(page_title='Average Movies', page_icon='popcorn.jpg')
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3blWZ-9USBzhL5-a_mJVhVFB5FfCjGoqz4A&s");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 movies_data = pd.read_csv("movies.csv")
 movies_data.dropna()
@@ -19,6 +35,7 @@ plt.xlabel('genre')
 plt.ylabel('budget')
 plt.title('Matplotlib Bar Chart Showing the Average \
 Budget of Movies in Each Genre')
+
 
 score_rating = movies_data['score'].unique().tolist()
 genre_list = movies_data['genre'].unique().tolist()
@@ -43,4 +60,34 @@ with st.sidebar:
                     year_list, 0)
 
 score_info = (movies_data['score'].between(*new_score_rating))
+new_genre_year = (movies_data['genre'].isin(new_genre_list)) \
+& (movies_data['year'] == year)
 
+# visualization section
+#group the columns needed for visualizations
+# col1, col2 = st.columns([2,3])
+# with col1:
+#     st.write("""#### Lists of movies filtered by year and Genre """)
+#     dataframe_genre_year = movies_data[new_genre_year]\
+#     .groupby(['name',  'genre'])['year'].sum()
+#     dataframe_genre_year = dataframe_genre_year.reset_index()
+#     st.dataframe(dataframe_genre_year, width = 400)
+#
+# with col2:
+#     st.write("""#### User score of movies and their genre """)
+#     rating_count_year = movies_data[score_info]\
+#     .groupby('genre')['score'].count()
+#     rating_count_year = rating_count_year.reset_index()
+#     figpx = px.line(rating_count_year, x = 'genre', y = 'score', width=100)
+#     st.plotly_chart(figpx)
+
+st.write("#### Movies Filtered by Year and Genre")
+dataframe_genre_year = movies_data[new_genre_year]\
+    .groupby(['name', 'genre'])['year'].sum().reset_index()
+st.dataframe(dataframe_genre_year, use_container_width=True, height=500)
+
+st.write("#### User Score Count by Genre")
+rating_count_year = movies_data[score_info]\
+    .groupby('genre')['score'].count().reset_index()
+figpx = px.line(rating_count_year, x='genre', y='score')
+st.plotly_chart(figpx, use_container_width=True)
